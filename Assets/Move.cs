9da79be206b7704f9b61e06ee13aed05dfbeeec9
Setoju <setoju48@gmail.com>
2025-05-04 17:12:59 +0300

    Initial commit.

---
+++ Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 1f;
+
+    void Start() {
+
+    }
+
+    private void LateUpdate() {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude < 0.1f) {
+            return;
+        }
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        this.transform.position = this.transform.position + velocity;
+    }
+}
